4c03fc9b81e4abcaa4d9652ced1a4745c840246c
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-06-25 01:47:17 +0600

    Use of Diff Data Type

---
+++ datatype.js
@@ -0,0 +1,36 @@
+// Primitive
+
+//string
+const name = 'Naf';
+
+//number
+const numb = 123;
+
+//boolean
+const isBD = true;
+
+//Null
+const car = null;
+
+//Undefined
+let test;
+
+//symbol
+const symb = Symbol();
+
+// Reference Types - Objects
+
+//array
+const arr = [1, 2, 3, 4];
+
+//object literal
+const obj = {
+    City: 'Dhaka',
+    Area: 'Mirpur'
+};
+
+//Date
+const today = new Date();
+
+console.log(today);
+console.log(typeof today);
