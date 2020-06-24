3ecc586b230fc1712051ae2bf1a86575e266d8c9
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-06-24 21:34:11 +0600

    Console Related JS

---
+++ console.js
@@ -0,0 +1,38 @@
+var greet = 'Hello World! This is a variable used alert.';
+var array = [1, 2, 3, 4];
+
+//using alert (prompt while loading page)
+alert(greet);
+
+//log to console
+//string
+console.log('Hello World');
+
+//number
+console.log(123);
+
+//boolean
+console.log(true);
+
+//variable
+console.log(greet);
+
+//array
+console.log(array);
+
+//object
+console.log({ a: 1, b: 2 });
+console.table({ a: 1, b: 2 });
+console.error('Error!');
+
+console.clear();
+console.warn('Everything in the console was cleared.')
+
+console.time(1);
+console.log(greet);
+console.log(greet);
+console.log(greet);
+console.log(greet);
+console.log(greet);
+console.log(greet);
+console.timeEnd(1);
