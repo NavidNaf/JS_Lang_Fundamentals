0f8a8d9bc32b6b76cb2de7ce7b8fdb6321e10373
NavidNaf <33064619+NavidNaf@users.noreply.github.com>
2020-06-25 11:47:06 +0600

    Math Operators

---
+++ math.js
@@ -0,0 +1,25 @@
+const num1 = 100;
+const num2 = 360;
+let val;
+
+//Simple Math
+val = num1 + num2;
+val = num1 * num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 % num2;
+
+//Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.floor(2.4);
+val = Math.sqrt(64);
+val = Math.abs(num1 - num2);
+val = Math.pow(7, 2);
+val = Math.min(2, 77, .05, -2, 55);
+val = Math.max(2, 77, .05, -2, 55);
+val = Math.random();
+val = Math.round(Math.random() * 100);
+
+console.log(val);
